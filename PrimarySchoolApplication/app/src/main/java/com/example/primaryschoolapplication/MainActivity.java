4c27282267583c79809ddf970ee343c3177f0387
April Hussey <april.hussey@outlook.com>
2023-03-15 12:21:15 +0000

    Fixed Login Now Works

--- PrimarySchoolApplication/app/src/main/java/com/example/primaryschoolapplication/MainActivity.java
+++ PrimarySchoolApplication/app/src/main/java/com/example/primaryschoolapplication/MainActivity.java
@@ -8,38 +8,53 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+{
+    EditText edtUsername;
+    EditText edtPassword;
+    Button btnLogin;
+    TextView txtDisplay;
+
     String strFixedUsername = "Username";
     String strFixedPassword = "Password";
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        EditText edtUsername = findViewByID(R.id.edtUsername);
-        EditText edtPassword = findViewByID(R.id.edtPassword);
-        Button btnLogin = findViewById(R.id.btnLogin);
-        TextView txtDisplay = findViewById(R.id.txtDisplay);
+        edtUsername = findViewById(R.id.edtUsername);
+        edtPassword = findViewById(R.id.edtPassword);
+        btnLogin = findViewById(R.id.btnLogin);
+        txtDisplay = findViewById(R.id.txtDisplay);
 
-        btnLogin.setOnClickListener(new View.OnClickListener() {
+        btnLogin.setOnClickListener(new View.OnClickListener()
+        {
             @Override
-            public void onClick(View view) {
+            public void onClick(View view)
+            {
                 String strUsername = edtUsername.getText().toString();
                 String strPassword = edtPassword.getText().toString();
 
                 User u1 = new User(strUsername, strPassword);
 
-                if (strUsername.isEmpty() || strPassword.isEmpty()) {
-                    txtDisplay.setText("Username or Password is empty");
-                } else {
-                    if (strUsername.equals(strFixedUsername) && strPassword.equals(strFixedPassword)) {
+                if (strUsername.isEmpty() || strPassword.isEmpty())
+                {
+                    txtDisplay.setText("Username or Password is incorrect");
+                }
+                else
+                {
+                    if (strUsername.equals(strFixedUsername) && strPassword.equals(strFixedPassword))
+                    {
                         txtDisplay.setText("Welcome");
-                    } else {
+                    }
+                    else
+                    {
                         txtDisplay.setText("Username or Password is incorrect");
                     }
                 }
             }
         });
     }
-        private EditText findViewByID(int edtUsername) { return null; }
 }
