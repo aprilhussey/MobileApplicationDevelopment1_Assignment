e7481bcbb9fdb37d4c1163fc24e1d2fea73d42d5
April Hussey <april.hussey@outlook.com>
2023-03-25 03:10:03 +0000

    Added TextViews and Buttons to DashboardActivity

--- PrimarySchoolApplication/app/src/main/java/com/example/primaryschoolapplication/DashboardActivity.java
+++ PrimarySchoolApplication/app/src/main/java/com/example/primaryschoolapplication/DashboardActivity.java
@@ -3,14 +3,26 @@ package com.example.primaryschoolapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class DashboardActivity extends AppCompatActivity
 {
+    TextView txtUserInfo;
+    TextView txtActivityTitle;
+    Button btnFileStorage;
+    Button btnQuiz;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
+
+        txtUserInfo = findViewById(R.id.txtUserInfo);
+        txtActivityTitle = findViewById(R.id.txtActivityTitle);
+        btnFileStorage = findViewById(R.id.btnFileStorage);
+        btnQuiz = findViewById(R.id.btnQuiz);
     }
 }
