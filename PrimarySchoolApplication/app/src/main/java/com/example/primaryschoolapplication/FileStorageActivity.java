031a8fc88a7573ae88199d8fa106af402bf5c72f
April Hussey <april.hussey@outlook.com>
2023-03-25 14:50:17 +0000

    Added FileStorage Activity

---
+++ PrimarySchoolApplication/app/src/main/java/com/example/primaryschoolapplication/FileStorageActivity.java
@@ -0,0 +1,16 @@
+package com.example.primaryschoolapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FileStorageActivity extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_file_storage);
+    }
+}
