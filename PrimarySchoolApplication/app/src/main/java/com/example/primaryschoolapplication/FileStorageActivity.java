56ffa63fadaacf47b3fe1b205b4cebf517535414
April Hussey <april.hussey@outlook.com>
2023-03-25 14:54:11 +0000

    Added TextViews, Buttons, and RecyclerView to FileStorage Activity

--- PrimarySchoolApplication/app/src/main/java/com/example/primaryschoolapplication/FileStorageActivity.java
+++ PrimarySchoolApplication/app/src/main/java/com/example/primaryschoolapplication/FileStorageActivity.java
@@ -1,16 +1,32 @@
 package com.example.primaryschoolapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class FileStorageActivity extends AppCompatActivity
 {
+    TextView txtUserInfo;
+    TextView txtActivityTitle;
+    RecyclerView recyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_file_storage);
+
+        User loggedInUser = LoginSystem.loggedInUser;
+
+        txtUserInfo = findViewById(R.id.txtUserInfo);
+        txtActivityTitle = findViewById(R.id.txtActivityTitle);
+        recyclerView = findViewById(R.id.recyclerView);
+
+        txtUserInfo.setText(loggedInUser.getUserID() + " - " + loggedInUser.getFirstName() + " " + loggedInUser.getLastName());
+
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));   // Arranges items in the recycler view in a vertical list
     }
 }
