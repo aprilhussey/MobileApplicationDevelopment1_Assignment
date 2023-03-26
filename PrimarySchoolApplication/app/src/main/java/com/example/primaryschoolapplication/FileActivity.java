2b29cb559a50c9d11a1c3824fde6f0a063136bf1
April Hussey <april.hussey@outlook.com>
2023-03-26 19:38:01 +0100

    Made Card Views Clickable and Added File Activity

---
+++ PrimarySchoolApplication/app/src/main/java/com/example/primaryschoolapplication/FileActivity.java
@@ -0,0 +1,15 @@
+package com.example.primaryschoolapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FileActivity extends AppCompatActivity
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_file);
+    }
+}
--- PrimarySchoolApplication/app/src/main/java/com/example/primaryschoolapplication/FileStorageActivity.java
+++ PrimarySchoolApplication/app/src/main/java/com/example/primaryschoolapplication/FileStorageActivity.java
@@ -1,10 +1,11 @@
 package com.example.primaryschoolapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -16,8 +17,7 @@ public class FileStorageActivity extends AppCompatActivity
     RecyclerView recyclerView;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_file_storage);
 
@@ -31,13 +31,23 @@ public class FileStorageActivity extends AppCompatActivity
 
         // Create new array list and add data to it
         ArrayList<FileModel> fileModelArrayList = new ArrayList<FileModel>();
-        fileModelArrayList.add (new FileModel("titleTest1", "blockTest1"));
-        fileModelArrayList.add (new FileModel("titleTest2", "blockTest2"));
+        fileModelArrayList.add(new FileModel("titleTest1", "blockTest1"));
+        fileModelArrayList.add(new FileModel("titleTest2", "blockTest2"));
 
         // Initialise adapter class and pass array list to it
         FileAdapter fileAdapter = new FileAdapter(this, fileModelArrayList);
 
         // Set adapter to recycler view
         recyclerView.setAdapter(fileAdapter);
+    }
+    public void onCardViewClick(View view)
+    {
+        TextView txtFileTitleView = (TextView) view.findViewById(R.id.txtFileTitle);
+        TextView txtFileBlockView = (TextView) view.findViewById(R.id.txtFileBlock);
+
+        Intent intentFile = new Intent(this, FileActivity.class);
+        intentFile.putExtra("fileTitle", txtFileTitleView.getText().toString());
+        intentFile.putExtra("fileBlock", txtFileBlockView.getText().toString());
+        startActivity(intentFile);
     }
 }
