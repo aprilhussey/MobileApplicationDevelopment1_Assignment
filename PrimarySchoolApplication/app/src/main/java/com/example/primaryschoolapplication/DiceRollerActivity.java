e0a04c5f887cacdd31240740f7c872e6f449983b
April Hussey <april.hussey@outlook.com>
2023-04-08 23:37:01 +0100

    Created Dice Roller Activity

---
+++ PrimarySchoolApplication/app/src/main/java/com/example/primaryschoolapplication/DiceRollerActivity.java
@@ -0,0 +1,16 @@
+package com.example.primaryschoolapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DiceRollerActivity extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dice_roller);
+    }
+}
